e8aa13503997aa3a3f5fb494c9cdfb99a259bcb8
Vibhaj Rajan <vibhaj8@gmail.com>
2016-03-02 01:12:41 +0530

    added s3 paths

--- lib/BrowserStack/Local.pm
+++ lib/BrowserStack/Local.pm
@@ -197,16 +197,16 @@ sub check_binary {
 
 sub platform_url {
   if ($^O =~ "darwin") {
-  return "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-darwin-x64";
+  return "https://s3.amazonaws.com/bs-automate-prod/local/BrowserStackLocal-darwin-x64";
   }
   elsif ($^O =~ /^Win/) {
-  return "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal.exe";
+  return "https://s3.amazonaws.com/bs-automate-prod/local/BrowserStackLocal-win32.exe";
   }
   if ($^O =~ "linux") {
   if (Config{'longsize'} == 8) {
-    return "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-x64";
+    return "https://s3.amazonaws.com/bs-automate-prod/local/BrowserStackLocal-linux-x64";
   } else {
-    return "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-ia32";
+    return "https://s3.amazonaws.com/bs-automate-prod/local/BrowserStackLocal-linux-ia32";
   }  
   }
 }
